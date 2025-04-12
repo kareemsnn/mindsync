f370331707252449ed0a2dbb3fc8791800920e69
Nathan Allen <nathan.noel.allen@gmail.com>
2025-04-12 14:59:59 -0400

    Initial supabase setup

---
+++ client/lib/supabase.ts
@@ -0,0 +1,27 @@
+import { createClient } from '@supabase/supabase-js'
+import { Database } from '@/database.types'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase environment variables');
+}
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+  realtime: {
+    params: {
+      eventsPerSecond: 10,
+    },
+  },
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  },
+  global: {
+    fetch: fetch.bind(globalThis),
+  },
+  db: {
+    schema: 'public',
+  },
+});
